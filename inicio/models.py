fdd318539eee2fe13ccd9e8ab2ebe060b0b6a939
jviejo@jviejo.com <damehueco2012>
2020-07-10 12:08:15 +0200

    jupyte para el proceso batch de frases

--- inicio/models.py
+++ inicio/models.py
@@ -1,12 +1,27 @@
+import requests
 from django.db import models
 import json
 
+class Frases(models.Model):
+    frase =  models.CharField(max_length=1000)
+    entidades = models.CharField(max_length=1000)
+    resultado = models.CharField(max_length=1000, null=True)
+
+    @staticmethod
+    def procesar():
+        for frase in Frases.objects.all():
+            json_data = {
+                "text": frase.frase,
+                "message_id": "11",
+            }
+            datos = requests.post('http://localhost:5005/model/parse/', json=json_data)
+            frase.resultado = datos.text
+            frase.save()
 
 class ChatModel(models.Model):
     usuario = models.CharField(max_length=100, null=True)
     texto = models.CharField(max_length=1000)
     fecha = models.DateTimeField(null=True, auto_now=True)
-
 
 
     def intencion(self):
